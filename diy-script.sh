0f6dd8c42d7685caaae92d82b36ce9a001cc9b64
goodmen001 <143695131+goodmen001@users.noreply.github.com>
2023-09-16 15:40:11 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -65,8 +65,8 @@ git clone https://github.com/kiddin9/openwrt-packages/tree/master/luci-app-cloud
 git clone --depth=1 https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
 svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 svn export https://github.com/haiibo/packages/trunk/luci-app-bypass package/luci-app-bypass
